f53f4adcfc8528421bfb4aff08904e90ff4d2709
leeseulbi0724 <tmfql4428@naver.com>
2021-04-15 15:59:27 +0900

    이진옥 16:00 User_MyPage_Usermodify (회원정보수정) - 미완료

--- Java_First_Project/Project/BookDAO/MemberDAO.java
+++ Java_First_Project/Project/BookDAO/MemberDAO.java
@@ -1,5 +1,31 @@
 package BookDAO;
 
-public class MemberDAO {
+import BookVO.MemberVO;
+
+public class MemberDAO extends DBConn{
 	
+	/** User_MyPage_Usermodify (사용자 - 회원정보수정) **/
+	public boolean getModifyResult(MemberVO member) {
+		// id 비교해서 맞으면 비밀번호, 폰번호, 주소 수정
+		boolean result = false;
+		
+		try {
+			String sql = " update book_users set pass = ?, hp = ?, addr = ? where id = ? "; //id 비교? pass 비교?
+			getPreparedStatement(sql);
+			
+			pstmt.setString(1, member.getPass());
+			pstmt.setString(2, member.getHp());
+			pstmt.setString(3, member.getAddr());
+			pstmt.setString(4, member.getId());
+			
+			int val = pstmt.executeUpdate();
+			if(val != 0) result = true;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+		
+	}
 }
--- Java_First_Project/Project/BookSystem/BookSystem.java
+++ Java_First_Project/Project/BookSystem/BookSystem.java
@@ -3,6 +3,7 @@ package BookSystem;
 import BookDAO.BookDAO;
 import BookDAO.MemberDAO;
 import BookVO.BookVO;
+import BookVO.MemberVO;
 
 public class BookSystem {
 			//Field
@@ -17,6 +18,11 @@ public class BookSystem {
 			/** 관리자 - 도서등록 **/
 			public boolean Admin_Insert(BookVO vo) {				
 				return bdao.getResultInsert(vo);
+			}
+			
+			/** 사용자 - 마이페이지 - 정보수정 **/
+			public boolean User_MyPage_Modify(MemberVO vo) {
+				return mdao.getModifyResult(vo);
 			}
 			
 }
