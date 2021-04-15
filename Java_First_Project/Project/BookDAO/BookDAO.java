22a15864d8d9ac0d3662ac549c5fa69e3066fccc
leeseulbi0724 <tmfql4428@naver.com>
2021-04-15 18:23:05 +0900

    4/15 18:22 (추천도서db연동)

--- Java_First_Project/Project/BookDAO/BookDAO.java
+++ Java_First_Project/Project/BookDAO/BookDAO.java
@@ -1,5 +1,9 @@
 package BookDAO;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 import BookVO.BookVO;
 
 public class BookDAO extends DBConn {
@@ -26,6 +30,36 @@ public class BookDAO extends DBConn {
 			e.printStackTrace();
 		}		
 		return result;
+	}
+	
+	//추천도서 누르면 텍스트필드에서 뜨는 데이터 받아오는 것
+	public String[][] recommendList() {
+		
+		try {
+			String sql = "SELECT*FROM BOOK_DATA WHERE BOOKNAME='정보처리기사'";
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet result = st.executeQuery();
+			
+			ArrayList<String[]> list = new ArrayList<String[]>();
+			
+			while(result.next()) {
+				list.add(new String[] {
+				String.valueOf(result.getInt("BNO")),
+				result.getString("BOOKNAME"),
+				result.getString("AUTHOR"),
+				result.getString("PBLSH"),
+				String.valueOf(result.getInt("PRICE")),
+				result.getString("PBLSHDATE")
+				});
+			}
+			
+			String[][] array = new String[list.size()][6];
+			return list.toArray(array);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 }
--- Java_First_Project/Project/BookUI/User_BookRecommendUI.java
+++ Java_First_Project/Project/BookUI/User_BookRecommendUI.java
@@ -3,15 +3,18 @@ package BookUI;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+import BookDAO.BookDAO;
+import BookDAO.DBConn;
 
 public class User_BookRecommendUI implements ActionListener {
 	
@@ -22,17 +25,23 @@ public class User_BookRecommendUI implements ActionListener {
 		JFrame f;
 		JButton lookReview_btn, putIn_btn;
 		JTextField bookNumber_tf,bookName_tf,bookAuthor_tf,publisher_tf,issueDay_tf,price_tf;	
+		BookDAO dao;
+		 
 		
 		//Constructor
 		
-		public User_BookRecommendUI(User_MainUI main) {
+		public User_BookRecommendUI(User_MainUI main){
 			this.main = main;
 			this.f= main.f;
 			init();
 		}	
 		//Method
 	
-		public void init() {
+		public void init(){
+		
+			dao = new BookDAO();
+			//BookDAO에 있는 메소드로 추천도서 db 데이터 받아오는 것
+			String[][] bookInfo = dao.recommendList();
 			
 			main.switching(User_MainUI.BOOK);		
 			
@@ -66,13 +75,13 @@ public class User_BookRecommendUI implements ActionListener {
 			publisherLabel.setBounds(180, 130, 69, 30);
 			bookPanel.add(publisherLabel);
 			
-			JLabel issueDayLabel = new JLabel("발행일");
-			issueDayLabel.setBounds(180, 170, 69, 30);
-			bookPanel.add(issueDayLabel);
-			
 			JLabel priceLabel = new JLabel("가격");
-			priceLabel.setBounds(180, 210, 69, 30);
+			priceLabel.setBounds(180, 170, 69, 30);
 			bookPanel.add(priceLabel);
+			
+			JLabel issueDayLabel = new JLabel("발행일");
+			issueDayLabel.setBounds(180, 210, 69, 30);
+			bookPanel.add(issueDayLabel);
 			
 			lookReview_btn = new JButton("리뷰보기");
 			lookReview_btn.setForeground(Color.WHITE);
@@ -86,41 +95,41 @@ public class User_BookRecommendUI implements ActionListener {
 			putIn_btn.setBounds(383, 268, 95, 47);
 			bookPanel.add(putIn_btn);
 			
-			bookNumber_tf = new JTextField();
+			bookNumber_tf = new JTextField(bookInfo[0][0]);
 			bookNumber_tf.setBounds(276, 10, 192, 30);
 			bookPanel.add(bookNumber_tf);
 			bookNumber_tf.setColumns(10);
 			bookNumber_tf.setEnabled(false);
 			
-			bookName_tf = new JTextField();
+			bookName_tf = new JTextField(bookInfo[0][1]);
 			bookName_tf.setColumns(10);
 			bookName_tf.setBounds(276, 50, 192, 30);
 			bookPanel.add(bookName_tf);
 			bookName_tf.setEnabled(false);
 			
-			bookAuthor_tf = new JTextField();
+			bookAuthor_tf = new JTextField(bookInfo[0][2]);
 			bookAuthor_tf.setColumns(10);
 			bookAuthor_tf.setBounds(276, 90, 192, 30);
 			bookPanel.add(bookAuthor_tf);
 			bookAuthor_tf.setEnabled(false);
 			
-			publisher_tf = new JTextField();
+			publisher_tf = new JTextField(bookInfo[0][3]);
 			publisher_tf.setColumns(10);
 			publisher_tf.setBounds(276, 130, 192, 30);
 			bookPanel.add(publisher_tf);
 			publisher_tf.setEnabled(false);
 			
-			issueDay_tf = new JTextField();
-			issueDay_tf.setColumns(10);
-			issueDay_tf.setBounds(276, 170, 192, 30);
-			bookPanel.add(issueDay_tf);
-			issueDay_tf.setEnabled(false);
-			
-			price_tf = new JTextField();
+			price_tf = new JTextField(bookInfo[0][4]);
 			price_tf.setColumns(10);
-			price_tf.setBounds(276, 210, 192, 30);
+			price_tf.setBounds(276, 170, 192, 30);
 			bookPanel.add(price_tf);
 			price_tf.setEnabled(false);
+			
+			issueDay_tf = new JTextField(bookInfo[0][5]);
+			issueDay_tf.setColumns(10);
+			issueDay_tf.setBounds(276, 210, 192, 30);
+			bookPanel.add(issueDay_tf);
+			issueDay_tf.setEnabled(false);
 			
 			lookReview_btn.addActionListener(this);
 			putIn_btn.addActionListener(this);
@@ -130,6 +139,7 @@ public class User_BookRecommendUI implements ActionListener {
 			
 			
 		}
+	
 	
 		//버튼 액션
 		public void actionPerformed(ActionEvent e) {
